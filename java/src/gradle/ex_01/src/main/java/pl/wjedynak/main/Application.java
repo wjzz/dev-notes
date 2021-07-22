1a1c09471dba90902ca6debe82d25d5ca52be741
wjzz <wjedynak@gmail.com>
2021-07-22 19:31:25 +0900

    More java examples

--- java/src/gradle/ex_01/src/main/java/pl/wjedynak/main/Application.java
+++ java/src/gradle/ex_01/src/main/java/pl/wjedynak/main/Application.java
@@ -3,5 +3,10 @@ package pl.wjedynak.main;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        Person person = new Person();
+        person.setName("Wojtek");
+        person.setAge(33);
+        System.out.println(person);
     }
 }
