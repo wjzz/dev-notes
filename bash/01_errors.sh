fa02cf9b409ed7da33623c1ba9a54234aa4cd8cd
Wojciech Jedynak <wjedynak@gmail.com>
2021-10-28 18:01:28 +0900

    bash

---
+++ bash/01_errors.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e # with this, the script stops after grep. without, we see the final 'end'
+
+echo "Start"
+
+grep
+
+echo "End"
---
+++ bash/02_errors.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+echo "Start"
+
+grep || echo "error"   # surpresses the error
+
+echo "End"
